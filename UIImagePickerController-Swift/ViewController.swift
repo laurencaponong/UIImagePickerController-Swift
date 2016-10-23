4538e705f7d26aad63fa303ea7e757645d01323d
Lauren <laurencaponong@gmail.com>
2016-10-23 17:39:06 -0400

    Setting up files & repository

---
+++ UIImagePickerController-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIImagePickerController-Swift
+//
+//  Created by Lauren Caponong on 10/23/16.
+//  Copyright © 2016 Lauren Caponong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

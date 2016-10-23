6b323b931d9789670f7ea521e74bbe867d0e68ed
laurencaponong <lcaponong@gmail.com>
2016-10-23 19:14:34 -0400

    Working UIImagePickerController

--- UIImagePickerController-Swift/ViewController.swift
+++ UIImagePickerController-Swift/ViewController.swift
@@ -8,18 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    //Variables
+    let imagePicker = UIImagePickerController()
+    
+    //Outlets
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func libraryTapped(_ sender: AnyObject) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func takePhotoTapped(_ sender: AnyObject) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            imagePicker.allowsEditing = false
+            imagePicker.sourceType = .camera
+            imagePicker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .camera)!
+            present(imagePicker, animated: true, completion: nil)
+        } else {
+            noCameraAlert()
+        }
+    }
+    
+    func noCameraAlert() {
+        let alertController = UIAlertController(title: "Error", message: "No camera available.", preferredStyle: .alert)
+        let okayAction = UIAlertAction(title: "Okay", style: .default, handler: nil)
+        alertController.addAction(okayAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        imagePicker.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK: - Delegate methods
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
     }
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        var chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        imageView.image = chosenImage
+        dismiss(animated: true, completion: nil)
+    }
 
 }
 
